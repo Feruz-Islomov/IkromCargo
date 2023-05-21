040290d9ef38fe6f67a85cce112be2216aef3134
Feruz <70520745+Feruz-Islomov@users.noreply.github.com>
2023-05-22 01:08:10 +0500

    table page

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 import { verify } from "jsonwebtoken";
 
-const secret = process.env.secret;
+const secret = process.env.SECRET;
 export default function middleware(req) {
   const { cookies } = req;
   const jwt = cookies.OursiteJWT;
