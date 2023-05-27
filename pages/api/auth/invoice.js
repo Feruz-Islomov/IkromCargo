ada5681bafc152e35a49f599020058551af5ef36
Feruz <70520745+Feruz-Islomov@users.noreply.github.com>
2023-05-28 01:20:26 +0500

    m2

--- pages/api/auth/invoice.js
+++ pages/api/auth/invoice.js
@@ -4,15 +4,19 @@ import path from "path";
 export default async function (req, res) {
   console.log(req.method);
   console.log(req.body);
-  if (req.method === "POST") {
-    const inv = req.body;
-    const filePath = path.join(process.cwd(), "/pages/api/invoice.json");
-    const fileData = fs.readFileSync(filePath);
-    const data = JSON.parse(fileData);
-    data.push(inv);
-    fs.writeFileSync(filePath, JSON.stringify(data));
-    res.status(200).json({ message: "Invoice qabul qilindi!" });
-  } else {
-    res.status(200).json({ message: "Method not allowed" });
+  try {
+    if (req.method === "POST") {
+      const inv = req.body;
+      const filePath = path.join(process.cwd(), "/pages/api/invoice.json");
+      const fileData = fs.readFileSync(filePath);
+      const data = JSON.parse(fileData);
+      data.push(inv);
+      fs.writeFileSync(filePath, JSON.stringify(data));
+      res.status(200).json({ message: "Invoice qabul qilindi!" });
+    } else {
+      res.status(200).json({ message: "Method not allowed" });
+    }
+  } catch (err) {
+    return res.json({ message: "Token is wrong!" });
   }
 }
